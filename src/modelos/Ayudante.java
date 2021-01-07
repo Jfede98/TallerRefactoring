488b1bdd2932594a2c77318b3bcb43b2bf63adac
TomVilla <tommyjoel20@gmail.com>
2021-01-07 13:00:30 -0500

    refactoring feature envy

--- src/modelos/Ayudante.java
+++ src/modelos/Ayudante.java
@@ -2,32 +2,16 @@ package modelos;
 
 import java.util.ArrayList;
 
-public class Ayudante {
-    protected Estudiante est;
+public class Ayudante extends Estudiante {
     public ArrayList<Paralelo> paralelos;
 
-    Ayudante(Estudiante e){
-    	est = e;
+    public ArrayList<Paralelo> getParalelos(){
+        return paralelos;
     }
-    public String getMatricula() {
-        return est.getMatricula();
-    }
-
-    public void setMatricula(String matricula) {
-        est.setMatricula(matricula);
-    }
-
-    //Getters y setters se delegan en objeto estudiante para no duplicar código
-    public String getNombre() {
-        return est.getNombre();
-    }
-
-    public String getApellido() {
-        return est.getApellido();
-    }
-
     //Los paralelos se añaden/eliminan directamente del Arraylist de paralelos
-
+    public void setParalelos(ArrayList<Paralelo> paralelos){
+        this.paralelos=paralelos;
+    }
 
     //Método para imprimir los paralelos que tiene asignados como ayudante
     public void MostrarParalelos(){
